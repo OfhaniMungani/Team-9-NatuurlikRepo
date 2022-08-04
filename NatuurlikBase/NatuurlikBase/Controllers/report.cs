8486fba76cc49011ea097bb54ec7eeb59b87be7f
Ofhani Mungani <u18022571@tuks.co.za>
2022-08-04 14:39:03 +0200

    Reporting chart reference example

---
+++ NatuurlikBase/NatuurlikBase/Controllers/report.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NatuurlikBase.Data;
+using Newtonsoft.Json;
+using NatuurlikBase.Models;
+using Microsoft.EntityFrameworkCore;
+using NatuurlikBase.ViewModels;
+
+namespace NatuurlikBase.Controllers
+{
+    public class report : Controller
+    {
+
+        private DatabaseContext db;
+
+        public report(DatabaseContext _db)
+        {
+            db = _db;
+        }
+
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public string GetOrderData()
+        {
+            List<userOrder> obj = db.OrderLine.Select(o => new userOrder
+            {
+                OrderID = o.OrderId,
+                Amount = (double)o.Price * o.Count,
+                UserID = o.Order.ApplicationUserId,
+            }).ToList();
+
+            List<object> list = new List<object>();
+            foreach (ApplicationUser user in db.User.ToList())
+            {
+                double amount = obj.Where(o => o.UserID == user.Id).Sum(o => o.Amount);
+                list.Add(new { Amount = Math.Round(amount), Name = user.FirstName + " " + user.Surname });
+            }
+
+            return JsonConvert.SerializeObject(list);
+        }
+        public string GetProductOrderData()
+        {
+            List<ProductOrder> obj = db.OrderLine.Select(o => new ProductOrder
+            {
+                OrderID = o.ProductId,
+                Amount = (double)o.Price * o.Count,
+
+            }).ToList();
+
+            List<object> list = new List<object>();
+            foreach (Product product in db.Products.ToList())
+            {
+                double amount = obj.Where(o => o.OrderID == product.Id).Sum(o => o.Amount);
+                list.Add(new { Amount = Math.Round(amount), Name = product.Name});
+            }
+
+            return JsonConvert.SerializeObject(list);
+        }
+    }
+
+
+}
+    
---
+++ NatuurlikBase/NatuurlikBase/ViewModels/userOrder.cs
@@ -0,0 +1,15 @@
+﻿namespace NatuurlikBase.ViewModels
+{
+    public class userOrder
+    {
+        public int OrderID { get; set; }
+        public double Amount { get; set; }
+        public string UserID { get; set; }
+    }
+    public class ProductOrder
+    {
+        public int OrderID { get; set; }
+        public double Amount { get; set; }
+       // public string UserID { get; set; }
+    }
+}
