6f0cee07dd41f0d503994d45b5a04e203343ad84
ChibsMimi <u17254061@tuks.gmail.com>
2022-08-12 15:14:16 +0200

    Customer and Reseller Reports

---
+++ NatuurlikBase/NatuurlikBase/Controllers/CustomerReport.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NatuurlikBase.Data;
+using NatuurlikBase.Models;
+
+namespace NatuurlikBase.Controllers
+{
+    public class CustomerReport : Controller
+    {
+
+        private readonly DatabaseContext _context;
+
+        public CustomerReport(DatabaseContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<ApplicationUser> users = (from user in _context.Users
+                                                  join userRole in _context.UserRoles on user.Id equals userRole.UserId
+                                                  join role in _context.Roles on userRole.RoleId equals role.Id
+                                                  where role.Name == "Customer"
+                                                  select user)
+                                                    .ToList();
+            ViewBag.count = users.Count();
+
+            return View(users);
+        }
+    }
+}
---
+++ NatuurlikBase/NatuurlikBase/Controllers/ResellerReport.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NatuurlikBase.Data;
+using NatuurlikBase.Models;
+
+namespace NatuurlikBase.Controllers
+{
+    public class ResellerReport : Controller
+    {
+
+        private readonly DatabaseContext _context;
+
+        public ResellerReport(DatabaseContext context)
+        {
+            _context = context;
+
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<ApplicationUser> users = (from user in _context.Users
+                                                  join userRole in _context.UserRoles on user.Id equals userRole.UserId
+                                                  join role in _context.Roles on userRole.RoleId equals role.Id
+                                                  where role.Name == "Reseller"
+                                                  select user)
+                               .ToList();
+
+            ViewBag.count = users.Count();
+
+            return View(users);
+        }
+    }
+}
