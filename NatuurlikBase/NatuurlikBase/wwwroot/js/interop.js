152db1a5191fe48218b055ef968c3abacfb1c5b0
Kyle Christiaan van Eeden <u18035176@tuks.co.za>
2022-08-13 18:03:34 +0200

    Export Production Report
    One form-control needs styling. Working on the claims on offline version, having some hiccups with retrieving auth user claims

---
+++ NatuurlikBase/NatuurlikBase/wwwroot/js/interop.js
@@ -0,0 +1,31 @@
+﻿
+function createAlert() {
+    alert('Hello');
+}
+
+
+function downloadPDF() {    
+    var header = document.querySelector('#head');
+    var doc = new jsPDF();
+    //HEADER
+    var width = doc.internal.pageSize.getWidth();
+    var height = doc.internal.pageSize.getHeight();
+    doc.addImage(header, 'png', 0, 8, width, height - 250)
+    //TITLE, DATE, PRINTED BY AND COUNT
+    doc.setFontSize(18);
+    doc.text(13, 60, "PRODUCTION REPORT");
+    doc.setFontSize(8);
+    var date = new Date().toLocaleDateString();
+    var printedby = "Kyle van Eeden";
+    doc.text(13, 66, "PRINTED BY: " + printedby);
+    doc.text(160, 66, "DATE PRINTED: " + date);
+    
+    //TABLE 
+    doc.autoTable( {html: '#tblData', startY:82, headStyles: {fillColor: [35, 35, 35]} });
+
+    //FOOTER, NEED TO ADD PAGE NUMBERS
+    doc.setFontSize(10);
+    let finalY = doc.autoTable.previous.finalY;
+    doc.text(88, finalY+20, "***END OF REPORT***");    
+        doc.save('NatuurlikProductionReport.pdf');
+    }
