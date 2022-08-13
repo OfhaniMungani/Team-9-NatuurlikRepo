49f3b0aef454b5bf663b784e242b1c51aff06679
ThenjiweN <thenjiwentsonda@gmail.com>
2022-08-13 15:30:34 +0200

    Product List Report

---
+++ NatuurlikBase/NatuurlikBase/Controllers/ProductReportController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NatuurlikBase.Data;
+using NatuurlikBase.Models;
+
+namespace NatuurlikBase.Controllers
+{
+    public class ProductReportController : Controller
+    {
+        public class ProductReportControllercs : Controller
+        {
+            private readonly DatabaseContext db;
+            public ProductReportControllercs(DatabaseContext _db)
+            {
+                db = _db;
+            }
+            public IActionResult Index()
+            {
+                IEnumerable<Product> products = db.Products.Include(b => b.Brand).Include(c => c.Category).ToList();
+                ViewBag.total = products.Sum(d => d.QuantityOnHand);
+
+                return View(products);
+            }
+        
+        
+        }
+    }
+}
