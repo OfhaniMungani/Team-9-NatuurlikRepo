5af9033afb84d1119d62dab5acd0d02b5fd84cf4
Kyle Christiaan van Eeden <u18035176@tuks.co.za>
2022-09-22 22:57:51 +0200

    Import Products from JSON
    Try catch to be implemented for duplicates and invalid JSON file structure

---
+++ NatuurlikBase/NatuurlikBase/Controllers/ImportProductCatalogueController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NatuurlikBase.Data;
+using NatuurlikBase.Models;
+using NatuurlikBase.Repository.IRepository;
+using Newtonsoft.Json;
+
+namespace NatuurlikBase.Controllers
+{
+    public class ImportProductCatalogueController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _hostEnvironment;
+        private readonly DatabaseContext _context;
+
+        public ImportProductCatalogueController(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment, DatabaseContext context)
+        {
+            _unitOfWork = unitOfWork;
+            _hostEnvironment = hostEnvironment;
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Import(IFormFile file)
+        {
+            using (var reader = new StreamReader(file.OpenReadStream()))
+            {
+                string json = reader.ReadToEnd();
+                List<Product> items = JsonConvert.DeserializeObject<List<Product>>(json);
+                foreach (var prod in items)
+                {
+                    _unitOfWork.Product.Add(prod);
+                }
+            }
+            _unitOfWork.Save();
+            TempData["success"] = "Products imported successfully";
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ NatuurlikBase/NatuurlikBase/Models/UploadFile.cs
@@ -0,0 +1,8 @@
+﻿namespace NatuurlikBase.Models
+{
+    public class UploadFile
+    {
+        public string Name { get; set; }
+        public IFormFile File { get; set; }
+    }
+}
